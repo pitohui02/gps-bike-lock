2285695fedd2d7127f83d24e1da3bbf29dd890d1
Pidel <fidelcarlos302@gmail.com>
2025-02-06 11:56:19 +0800

    Finished popup for logout and added a "Legal Information" in the bottom part of the sidebar menu. Added a shadow design for all buttons.

--- src/components/Popup-Logout.tsx
+++ src/components/Popup-Logout.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { IonModal, IonButton, IonContent, IonHeader, IonToolbar, IonTitle, IonButtons, IonText } from '@ionic/react';
-
+import './Popup-Logout.css'
+import logo from '../assets/lockate-logo.svg'
 const PopupLogout: React.FC<{ isOpen: boolean; onClose: () => void }> = ({ isOpen, onClose }) => {
   return (
     <div className='main-cont-popup'>
         <IonModal isOpen={isOpen} onDidDismiss={onClose} className="logout-modal">
-        <IonContent className="ion-padding">
+
+            <div className='app-logo'>
+              <img src={logo} alt='app-logo'/>
+            </div>
+
             <IonText className='alert-txt'>
             Are you sure?
             </IonText>
-            <IonButton expand="block" onClick={() => { console.log('Logout confirmed'); onClose(); }}>Logout</IonButton>
-            <IonButton expand="block" color="medium" onClick={onClose}>Cancel</IonButton>
-        </IonContent>
+
+            <div className='cancel-logout'>
+              <IonButton className='cancel-btn' expand="block" onClick={onClose}>Cancel</IonButton>
+
+              <IonButton className='logout-btn' expand="block" onClick={() => { console.log('Logout confirmed'); onClose(); }}>Logout</IonButton>
+
+            </div>
         </IonModal>
     </div>
   );
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,51 +1,5 @@
-// // Sidebar.tsx
-// import React, { useState } from 'react';
-// import { IonContent, IonHeader, IonMenu, IonList, IonItem } from '@ionic/react';
-// import PopupLogout from '../components/Popup-Logout';
-// import './Sidebar.css';
-// import logout from "../assets/logout-txt-sb.svg";
-// import about from "../assets/about-txt-sb.svg";
-// import ridehistory from "../assets/ridehistory-txt-sb.svg";
-// import mybike from "../assets/mybike-txt-sb.svg";
-
-// const Sidebar: React.FC = () => {
-//   const [showLogoutAlert, setShowLogoutAlert] = useState(false);
-
-//   return (
-//     <div className='main-container-sb'>
-//       <IonMenu side="end" contentId="main-content" type="overlay" className='main-sidebar-cont'>
-//         <div className='menu-content'>
-//           <IonHeader />
-//           <IonList className='item-list'>
-
-//             <IonItem detail-icon="none" lines='none' className='my-bike' button onClick={() => console.log('My Bike Clicked')}>
-//               <img src={mybike} alt='My Bike'/>
-//             </IonItem>
-
-//             <IonItem detail-icon="none" lines='none' className='ride-history' button onClick={() => console.log('Ride History Clicked')}>
-//               <img src={ridehistory} alt='Ride History'/>
-//             </IonItem>
-
-//             <IonItem detail-icon="none" lines='none' className='about' button onClick={() => console.log('About Clicked')}>
-//               <img src={about} alt='About'/>
-//             </IonItem>
-
-//             <IonItem detail-icon="none" lines='none' className='log-out-btn' button onClick={() => setShowLogoutAlert(true)}>
-//               <img src={logout} alt='Logout Button'/>
-//             </IonItem>
-
-//           </IonList>
-//         </div>
-//       </IonMenu>
-//       <PopupLogout isOpen={showLogoutAlert} onDidDismiss={() => setShowLogoutAlert(false)} />
-//     </div>
-//   );
-// };
-
-// export default Sidebar;
-
 import React, { useState } from 'react';
-import { IonContent, IonHeader, IonMenu, IonTitle, IonToolbar, IonList, IonItem, IonIcon, IonLabel } from '@ionic/react';
+import { IonText, IonContent, IonHeader, IonMenu, IonTitle, IonToolbar, IonList, IonItem, IonIcon, IonLabel } from '@ionic/react';
 import PopupLogout from '../components/Popup-Logout'; 
 import './Sidebar.css';
 import logout from "../assets/logout-txt-sb.svg";
@@ -65,31 +19,35 @@ const Sidebar: React.FC = () => {
               <IonTitle>Menu</IonTitle>
             </IonToolbar>
           </IonHeader>
-            <IonList className='item-list'>
+          <IonList className='item-list'>
+            <IonItem detail-icon="none" lines='none' className='my-bike' button onClick={() => { console.log('My Bike Clicked'); }}>
+              <img src={mybike} alt='My Bike'/>
+            </IonItem>
 
-                <IonItem detail-icon="none" lines='none' className='my-bike' button onClick={() => { console.log('My Bike Clicked'); }}>
-                  <img src={mybike} alt='My Bike'/>
-                </IonItem>
+            <IonItem detail-icon="none" lines='none' className='ride-history' button onClick={() => { console.log('Ride History Clicked'); }}>
+              <img src={ridehistory} alt='Ride History'/>
+            </IonItem>
 
-                <IonItem detail-icon="none" lines='none' className='ride-history' button onClick={() => { console.log('Ride History Clicked'); }}>
-                  <img src={ridehistory} alt='Ride History'/>
-                </IonItem>
+            <IonItem detail-icon="none" lines='none' className='about' button onClick={() => { console.log('About Clicked'); }}>
+              <img src={about} alt='About'/>
+            </IonItem>
 
-                <IonItem detail-icon="none" lines='none' className='about' button onClick={() => { console.log('About Clicked'); }}>
-                  <img src={about} alt='About'/>
-                </IonItem>
-
-                <IonItem detail-icon="none" lines='none' className='log-out-btn' button onClick={() => setShowLogoutModal(true)}>
-                  <img src={logout} alt='Logout button'/>
-                </IonItem>
-
-            </IonList>
+            <IonItem detail-icon="none" lines='none' className='log-out-btn' button onClick={() => setShowLogoutModal(true)}>
+              <img src={logout} alt='Logout button'/>
+            </IonItem>
+          </IonList>
         </div>
+        <div className='legal-info'> 
+          <IonText>
+            <span className='legal'>Legal Information</span> 
+            <br/> V 1.0
+          </IonText>
+        </div> 
       </IonMenu>
       <PopupLogout isOpen={showLogoutModal} onClose={() => setShowLogoutModal(false)} />
+
     </div>
   );
 };
 
 export default Sidebar;
-
